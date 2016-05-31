c64c133f5e520babc4cd561da4429d1a83802374
ernstoliver <ernstoliver94@gmail.com>
2016-05-31 09:30:10 +0200

    Tested OK !

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,10 +1,21 @@
-def palindrome(str):
-    return
-
+def palindrome(pal):
+    pal2 = pal.replace(" ", "")
+    pal3 = pal2.lower()
+    #your_input = palindrome(pal3)
+    if str(pal3) == str(pal3)[::-1]:
+        return True
+    else:
+        return False
 
 def main():
-    return
-
+    pal = input("Write something:")
+    palin_drome = palindrome(pal)
+    if palin_drome == True:
+        print("Your string is a palindrome")
+        return True
+    else:
+        print("Your string does not a palindrome")
+        return False
 
 if __name__ == '__main__':
     main()
