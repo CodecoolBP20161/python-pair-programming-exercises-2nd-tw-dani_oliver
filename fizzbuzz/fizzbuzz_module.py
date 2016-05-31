48b0af7e2ea5585a5d65d7e5daec2d51846cfe9b
ernstoliver <ernstoliver94@gmail.com>
2016-05-31 09:29:15 +0200

    Tested OK

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,8 +1,19 @@
 def fizzbuzz(number):
-    return
+    to_return = number
+    if number%15 == 0:
+        to_return = 'FizzBuzz'
+    elif number%5 == 0:
+        to_return = 'Buzz'
+    elif number%3 == 0:
+        to_return = 'Fizz'
+
+    return to_return
 
 
 def main():
+    for i in range(1,101):
+        num = fizzbuzz(i)
+        print(num)
     return
 
 if __name__ == '__main__':
